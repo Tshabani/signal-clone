5bd46928974744be6ac5156bc06629dc39a8020a
Moses <matimba.moses@boxfusion.io>
2021-07-04 23:09:03 +0200

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_pS-EOht1fYwyk4qdWk3aV7f0ihP9YNw",
+    authDomain: "signal-clone-5c4cc.firebaseapp.com",
+    projectId: "signal-clone-5c4cc",
+    storageBucket: "signal-clone-5c4cc.appspot.com",
+    messagingSenderId: "128056463769",
+    appId: "1:128056463769:web:10e7c3f565957f009f8308"
+  };
